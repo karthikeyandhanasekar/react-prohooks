12cfa39e8cc52ad2a35cbd9da7db40f16cb92ab2
Karthik <dkkarthik2000@gmail.com>
2021-12-23 16:39:42 +0530

    completed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,3 @@
-import { wait } from "@testing-library/user-event/dist/utils"
 import axios from "axios"
 import React from "react"
 
@@ -6,7 +5,7 @@ import React from "react"
 
 const Search = () => {
     const [search, setsearch] = React.useState('')
-    const [result, setresult] = React.useState()
+    const [results, setresult] = React.useState()
 
 
     const searchbook = async (name) => {
@@ -24,7 +23,15 @@ const Search = () => {
         if (search !== '')
             searchbook(search)
     }, [search])
-    console.log(result);
+    results?.map(ele => console.log(ele))
+    const ui = results?.map(ele =>
+
+        <tr>
+            <td>{ele.title}</td>
+            <td>{ele.author_name ? ele.author_name : "N/A"}</td>
+            <td>{ele.first_publish_year? ele.first_publish_year : "N/A"}</td>
+        </tr>
+    )
     return (
         <React.Fragment>
             <input type="search" placeholder="Search" value={search} onChange={(e) => setsearch(e.target.value)} autoFocus={true} />
@@ -37,6 +44,9 @@ const Search = () => {
                         <th>Pub. Year</th>
                     </tr>
                 </thead>
+                <tbody>
+        {ui}
+                </tbody>
             </table>
 
         </React.Fragment>
@@ -45,3 +55,45 @@ const Search = () => {
 
 
 export default Search
+
+/**
+ * author_facet: Array(1)
+0: "OL2761571A NA"
+length: 1
+[[Prototype]]: Array(0)
+author_key: Array(1)
+0: "OL2761571A"
+length: 1
+[[Prototype]]: Array(0)
+author_name: Array(1)
+0: "NA"
+length: 1
+[[Prototype]]: Array(0)
+cover_edition_key: "OL32479081M"
+cover_i: 11134684
+ebook_count_i: 0
+edition_count: 1
+edition_key: Array(1)
+0: "OL32479081M"
+length: 1
+[[Prototype]]: Array(0)
+first_publish_year: 2001
+has_fulltext: false
+isbn: (2) ['9780130765345', '0130765341']
+key: "/works/OL24509399W"
+last_modified_i: 1621550719
+publish_date: ['Oct 01, 2001']
+publish_year: [2001]
+publisher: ['Addison Wesley Longman']
+publisher_facet: ['Addison Wesley Longman']
+seed: Array(3)
+0: "/books/OL32479081M"
+1: "/works/OL24509399W"
+2: "/authors/OL2761571A"
+length: 3
+[[Prototype]]: Array(0)
+title: "Chem&life"
+title_suggest: "Chem&life"
+type: "work"
+_version_: 1715088258836201500
+ */
